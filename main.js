405f3b34d9efabe0a3b406ce96119c27d15c46aa
cmj <chmin82@gmail.com>
2020-12-13 22:17:48 +0900

    My SQL로 글생성 기능 구현

--- main.js
+++ main.js
@@ -83,11 +83,12 @@ var app = http.createServer(function(request,response){
         });
       }
     } else if(pathname === '/create'){
-      fs.readdir('./data', function(error, filelist){
-        var title = 'WEB - create';
-        var list = template.list(filelist);
-        var html = template.HTML(title, list, `
-          <form action="/create_process" method="post">
+      db.query(`SELECT * FROM topic`, (err, res) => {
+        console.log(res);
+        var title = 'Create';
+        var list = template.list(res);
+        var html = template.HTML(title, list,
+          `<form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
               <textarea name="description" placeholder="description"></textarea>
@@ -95,11 +96,28 @@ var app = http.createServer(function(request,response){
             <p>
               <input type="submit">
             </p>
-          </form>
-        `, '');
+          </form>`,
+           `<a href="/create">create</a>`);
         response.writeHead(200);
         response.end(html);
       });
+      // fs.readdir('./data', function(error, filelist){
+      //   var title = 'WEB - create';
+      //   var list = template.list(filelist);
+      //   var html = template.HTML(title, list, `
+      //     <form action="/create_process" method="post">
+      //       <p><input type="text" name="title" placeholder="title"></p>
+      //       <p>
+      //         <textarea name="description" placeholder="description"></textarea>
+      //       </p>
+      //       <p>
+      //         <input type="submit">
+      //       </p>
+      //     </form>
+      //   `, '');
+      //   response.writeHead(200);
+      //   response.end(html);
+      // });
     } else if(pathname === '/create_process'){
       var body = '';
       request.on('data', function(data){
@@ -109,10 +127,16 @@ var app = http.createServer(function(request,response){
           var post = qs.parse(body);
           var title = post.title;
           var description = post.description;
-          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-            response.writeHead(302, {Location: `/?id=${title}`});
-            response.end();
-          })
+          // fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+          //   response.writeHead(302, {Location: `/?id=${title}`});
+          //   response.end();
+          // });
+          db.query(`INSERT INTO topic (title, description, created, author_id)
+            VALUES (?, ?, NOW(), ?)`, 
+            [post.title, post.description, 1], (err, results) => {
+              response.writeHead(302, {Location: `/?id=${results.insertId}`});
+              response.end();
+            });
       });
     } else if(pathname === '/update'){
       fs.readdir('./data', function(error, filelist){
