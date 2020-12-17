61105e35e2c796f081eec36aa848669d6546ca07
cmj <chmin82@gmail.com>
2020-12-17 22:23:34 +0900

    Node.js 코드의 정리정돈 (topic)

--- main.js
+++ main.js
@@ -24,81 +24,11 @@ var app = http.createServer(function(request, response){
     } else if(pathname === '/create_process'){
       topic.create_process(request, response);
     } else if(pathname === '/update'){
-      db.query('SELECT * FROM topic', (err, topics) => {
-      // fs.readdir('./data', function(error, filelist){
-        if(err) {
-          throw err;
-        }
-        db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id], (err2, topic) => {
-          if(err2) {
-            throw err2;
-          }
-          db.query(`SELECT * FROM author`, (err2, authors) => {
-            var list = template.list(topics);
-            var html = template.HTML(topic[0].title, list,
-            `<form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${topic[0].id}">
-              <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${topic[0].description}</textarea>
-              </p>
-              <p>
-                ${template.authorSelect(authors, topic[0].author_id)}
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `,
-            `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
-            );
-            response.writeHead(200);
-            response.end(html);
-          });
-        });
-      });
+      topic.update(request, response);
     } else if(pathname === '/update_process'){
-      var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);
-          // var id = post.id;
-          // var title = post.title;
-          // var description = post.description;
-          // fs.rename(`data/${id}`, `data/${title}`, function(error){
-          //   fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-          //     response.writeHead(302, {Location: `/?id=${title}`});
-          //     response.end();
-          //   })
-          // });
-          db.query(`UPDATE topic SET title=?, description=?, author_id=? WHERE id=?`, [post.title, post.description, post.author, post.id], (err, result) => {
-            response.writeHead(302, {Location: `/?id=${post.id}`});
-            response.end();
-          })
-      });
+      topic.update_process(request, response);
     } else if(pathname === '/delete_process'){
-      var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);
-          var id = post.id;
-          var filteredId = path.parse(id).base;
-          db.query(`DELETE FROM topic WHERE id = ?`, [post.id], (err, result) => {
-            if(err) {
-              throw err;
-            }
-            response.writeHead(302, {Location: `/`});
-            response.end();
-          });
-          // fs.unlink(`data/${filteredId}`, function(error){
-          //   response.writeHead(302, {Location: `/`});
-          //   response.end();
-          // })
-      });
+      topic.delete_process(request, response);
     } else {
       response.writeHead(404);
       response.end('Not found');
